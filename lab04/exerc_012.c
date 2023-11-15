fe81bf0ff117604e2a4f1c4438f6465380dbc9c7
Caio Pereira Neris <caiofeliz14@hotmail.com>
2023-11-15 14:40:36 -0300

    eita

--- lab04/exerc_012.c
+++ lab04/exerc_012.c
@@ -63,15 +63,15 @@ void verifica(VectorInt v, void* a){
 
 }
 
-void* procura(VectorInt v, int i){
-    int numero = -1;
-    //if (v->capacity-1 >= i){
-    if (v->nelements > i){
-        numero = v->data[i];
+void* procura(VectorInt v, int i) {
+    void* elemento = NULL;
+
+    // Verifica se a posição i é válida no vetor
+    if (i >= 0 && i < v->nelements) {
+        elemento = v->data[i];
     }
 
-    return numero;
-
+    return elemento;
 }
 
 int conta(VectorInt v){
@@ -134,46 +134,132 @@ void limpa_memoria(VectorInt v){
     free(v);
 }
 
+// int cria_int(){
+//     int* a = malloc(sizeof(int));
+    
+//     return a;
+// }
+
 int main(){
-    int* a = malloc(sizeof(int));
-    int* i = malloc(sizeof(int));
+    void *a;
+    int i;
 
     VectorInt v = vectorint();
     int comando = 0;
 
-    printf("Voce pode escolher a funcao que deseja fazer por comandos\n");
+    printf("Digite 6 para que seu proximo dado seja do tipo int, 7 para que seja char ou 8 para que seja string\n");
+    printf("\n");
+    printf("Para inserir, digite 1. Para remover todas ocorrencias, digite 2\n");
+    printf("Para saber se pertence ao vetor, digite 3, para retornar da iesima posicao, digite 4\n");
+    printf("Para saber a quantidade atual de elementos, digite 5, para encerrar o programa e liberar a memoria, digite -1\n");
+    printf("\n");
 
-    printf("Para inserir um numero no vetor, digite 1. Para remover todas ocorrencias de um numero do vetor, digite 2\n");
-    printf("Para saber se um numero pertence ao vetor, digite 3, para retornar um elemento da iesima posicao, digite 4\n");
-    printf("Para saber a quantidade atual de elementos no vetor, digite 5, para encerrar o programa e liberar a memoria, digite -1\n");
-
+    int m, comando_tipo;
 
     while (comando != -2){
-        printf("\n");
-        printf("Digite um comando\n");
+        printf("Digite o comando que explicite qual o tipo do dado a ser inserido / analisado \n");
 
+        scanf("%d", &comando_tipo);
+
+        if (comando_tipo == 6){
+            int* a_int = malloc(sizeof(int));
+            scanf("%d", a_int);
+            a = (void*)a_int;
+
+        } else if (comando_tipo == 7){
+            char* a_char = malloc(sizeof(char));
+            scanf(" %c", a_char);  
+            a = (void*)a_char;
+            
+        } else if(comando_tipo == 8){
+            printf("Qual tamanho dessa string? \n");
+            scanf("%d", &m);
+            char* a_str = malloc((m + 1) * sizeof(char));
+            scanf("%s", a_str);
+            a = (void*)a_str;
+        }
+        
+        printf("Digite o que deseja fazer agora\n");
         scanf("%d", &comando);
 
-        if (comando == 1){
-            printf("Digite o numero que deseja adicionar\n");
-            scanf("%d", &a);
-            vectorint_insert(v, (void*) a);
+if (comando == 1) {
+    printf("Digite o que deseja adicionar\n");
 
-        } else if(comando == 2){ 
-            printf("Digite o elemento que quer remover todas ocorrencias: \n");
-            scanf("%d", &a);
-            vectorint_remove(v, (void*) a);
+    if (comando_tipo == 6) {
+        int* a_int = malloc(sizeof(int));
+        scanf("%d", a_int);
+        a = (void*)a_int;
+    } else if (comando_tipo == 7) {
+        char* a_char = malloc(sizeof(char));
+        scanf(" %c", a_char);  // Adicionado espaço antes de %c para ignorar espaços em branco
+        a = (void*)a_char;
+    } else if (comando_tipo == 8) {
+        printf("Digite a string: \n");
+        char* a_str = malloc((m + 1) * sizeof(char));
+        fgets(a_str, m + 1, stdin);  // m + 1 para incluir o caractere de nova linha
+        a = (void*)a_str;
+    }
 
-        }else if (comando == 3){
-            printf("Digite o numero que voce quer descobrir se pertence ao vetor: \n");
-            scanf("%d", &a);
-            verifica(v, (void*) a);
+    vectorint_insert(v, a);
 
-        } else if (comando == 4){
-            printf("Digite a posicao, se retornar -1 e porque nao existe essa posicao, se encontrar, vai imprimir o numero\n");
+} else if (comando == 2) {
+    printf("Digite o elemento que quer remover todas ocorrencias: \n");
+
+    if (comando_tipo == 6) {
+        int a_int;
+        scanf("%d", &a_int);
+        a = (void*)&a_int;
+    } else if (comando_tipo == 7) {
+        char a_char;
+        scanf(" %c", &a_char);
+        a = (void*)&a_char;
+    } else if (comando_tipo == 8) {
+        printf("Digite a string: \n");
+        char a_str[m + 1];
+        fgets(a_str, m + 1, stdin);
+        a = (void*)a_str;
+    }
+
+    vectorint_remove(v, a);
+
+} else if (comando == 3) {
+    printf("Digite o que voce quer descobrir se pertence ao vetor: \n");
+
+    if (comando_tipo == 6) {
+        int a_int;
+        scanf("%d", &a_int);
+        a = (void*)&a_int;
+    } else if (comando_tipo == 7) {
+        char a_char;
+        scanf(" %c", &a_char);
+        a = (void*)&a_char;
+    } else if (comando_tipo == 8) {
+        printf("Digite a string: \n");
+        char a_str[m + 1];
+        fgets(a_str, m + 1, stdin);
+        a = (void*)a_str;
+    }
+
+    verifica(v, a);
+
+        } else if (comando == 4) {
+            printf("Digite a posicao, se retornar NULL e porque nao existe essa posicao, se encontrar, vai imprimir o que esta nela\n");
             scanf("%d", &i);
-            int pos = procura(v,(void*) i);
-            printf("%d \n", pos);
+            
+            void* resultado = procura(v, i);
+
+            if (resultado != NULL) {
+
+                if (comando_tipo == 6) {
+                    printf("%d\n", *((int*)resultado));
+                } else if (comando_tipo== 7) {
+                    printf("%c\n", *((char*)resultado));
+                } else if (comando_tipo == 8) {
+                    printf("%s\n", (char*)resultado);
+                }
+            } else {
+                printf("Posicao nao existe no vetor.\n");
+            }
 
         } else if (comando ==5){
             int n_element = conta(v);
@@ -182,7 +268,7 @@ int main(){
             printf("A memoria total foi liberada e o programa vai encerrar \n");
             limpa_memoria(v);
             comando = -2;
-        }
+        } 
     
     }
 
