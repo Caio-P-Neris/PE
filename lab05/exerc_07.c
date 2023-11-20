9f0e7e69816901017ec43def6c645cea93963d12
caio neris <caiofeliz14@hotmail.com>
2023-11-20 12:38:15 -0300

    ex7

--- lab05/exerc_07.c
+++ lab05/exerc_07.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int func(int n, int k){
-    if (k == 0){
-        return 1;
-    } else{
-        return func(n-1, k-1) + func(n-1, k);
+    while (n >= k && n >= 0){
+        if (k == 0 || n ==k){
+            return 1;
+        } else{
+            return func(n-1, k-1) + func(n-1, k);
+        }
     }
-
 }
 
 int main(){
@@ -18,5 +19,6 @@ int main(){
     scanf("%d", &k);
 
     int resp = func(n,k);
+    printf("resultado : %d \n", resp);
 
 }
